fc1678c4168ca3b919501c79200bd3d6b8b0c5dd
lukef7fywmrp <lukef7fywmrp@gmail.com>
2025-10-05 21:19:14 +0400

    fix undefined issue

--- app/api/geo/route.ts
+++ app/api/geo/route.ts
@@ -3,5 +3,5 @@ import { geolocation } from "@vercel/functions";
 export function GET(request: Request) {
   const geo = geolocation(request);
 
-  return new Response(geo.country || "US");
+  return new Response(geo?.country || "US");
 }
