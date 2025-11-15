919ee04b65498e9f68e54d43f4d5ee31a2e56efd
lukef7fywmrp <lukef7fywmrp@gmail.com>
2025-11-15 14:58:53 +0400

    remove import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import ConvexClientProvider from "@/components/ConvexClientProvider";
 import QueryClientProvider from "@/components/QueryClientProvider";
 import { Toaster } from "@/components/ui/sonner";
 import { ClerkProvider } from "@clerk/nextjs";
-import type { Metadata, Viewport } from "next";
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
